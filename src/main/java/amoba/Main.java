2b8c6a7ed3c1fff2391f32b38f78a7c08b32278e
snr <sandor.szmolen@gmail.com>
2025-11-05 10:14:10 +0100

    Player, enum (X vagy O) + rövid teszt a pozíció visszaadásra és player váltásra

--- src/main/java/amoba/Main.java
+++ src/main/java/amoba/Main.java
@@ -2,7 +2,30 @@ package amoba;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("mainrun / clean test");
-        System.out.println("mainrun / clean test");
+
+        // Player teszt
+        System.out.println("Player teszt");
+        Player current = Player.X; // X kezd
+        System.out.println("Kezdő játékos: " + current);
+
+        for (int i = 0; i < 5; i++) {
+            current = current.next(); // váltás a másik játékosra
+            System.out.println("Következő játékos: " + current);
+        }
+
+        System.out.println("\nPosition teszt");
+        Position p1 = new Position(0, 0);
+        System.out.println("Kézzel létrehozott pozíció: " + p1);
+
+        Position p2 = Position.converter("a1");
+        System.out.println("Szövegből ('b3') konvertált pozíció: " + p2);
+
+        if (p1.equals(p2)) {
+            System.out.println("p1 és p3 egyenlő (ugyanaz a sor és oszlop) " + p1);
+        } else {
+            System.out.println("p1 és p3 különböző pozíció " + p1 + " " + p2);
+        }
+
+        System.out.println("\nTeszt vége");
     }
 }
